b70c4b91bad4bd55232088764b1bd4223c2c3de1
Cedrik <cedrik@arweave.org>
2020-05-13 20:20:51 -0500

    cluster memory cache

--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -1,31 +1,73 @@
 import { Request, Response, NextFunction } from 'express';
-import mcache from 'memory-cache';
+import memored from 'memored';
 import RedisCache from 'node-cache-redis';
 
-let client = null;
-try {
-    client = new RedisCache({
-        redisOptions: {
-            url: process.env.REDIS_URL
-        }
-    });
-    client.deleteAll();
-} catch(e) {
-    console.log(e);
-}
+export class Cache {
+    private isRedis: boolean = false;
+    private _client: any;
+    private ttl: number;
 
-const cacheMiddleware = (ttl: number) => async (req: Request, res: Response, next: NextFunction) => {
-    let key = `__express__${req.originalUrl}` || req.url;
-    let cachedBody = null;
-    try {
-        cachedBody = await client.get(key);
-    } catch(e) {
-        console.log(e);
-        cachedBody = mcache.get(key);
+    public get client() {
+        return this._client;
     }
 
-    if(cachedBody) {
-        return res.send(cachedBody);
+    constructor(ttl: number = (60*5)) {
+        this.ttl = ttl;
+
+        try {
+            this._client = new RedisCache({
+                redisOptions: {
+                    url: process.env.REDIS_URL
+                }
+            });
+            this._client.deleteAll();
+            this.isRedis = true;
+        } catch(e) {
+            this._client = memored;
+            //this._client.setup({ purgeInterval: (1000*60*60)});
+            this.isRedis = false;
+            console.log(e);
+        }
+    }
+
+    async set(key: string, data: any, ttl: number = 0): Promise<boolean> {
+        const tmpTtl = (ttl || this.ttl) * 1000;
+        if(this.isRedis) {
+            return await this._client.set(key, data, tmpTtl);
+        } 
+        
+        return new Promise((resolve, reject) => {
+            this._client.store(key, data, tmpTtl, (err) => {
+                if(err) {
+                    return reject(err);
+                }
+
+                return resolve(data);
+            });
+        });
+    }
+
+    async get(key: string): Promise<any> {
+        if(this.isRedis) {
+            return await this._client.get(key);
+        }
+        return new Promise((resolve, reject) => {
+            this._client.read(key, (err, data) => {
+                if(err || !data || !data.length) return reject(err);
+
+                return data;
+            });
+        });
+    }
+}
+
+const cache = new Cache(60 * 60);
+const cacheMiddleware = (ttl: number) => async (req: Request, res: Response, next: NextFunction) => {
+    let key = `__express__${req.originalUrl}` || req.url;
+    try {
+        return res.send(await cache.get(key));
+    } catch(e) {
+        console.log(e);
     }
 
     // @ts-ignore
@@ -33,10 +75,9 @@ const cacheMiddleware = (ttl: number) => async (req: Request, res: Response, nex
     // @ts-ignore
     res.send = async (body) => {
         try {
-            await client.set(key, body, ttl);
+            await cache.set(key, body);
         } catch(e) {
             console.log(e);
-            mcache.put(key, body, (ttl * 1000));
         }
         
         // @ts-ignore
